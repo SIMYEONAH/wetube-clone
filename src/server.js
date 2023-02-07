50e2213555439c0185c49fb557c3fedef26c6333
SIMYEONAH <simya0528@naver.com>
2023-02-08 00:21:05 +0900

    server update

--- src/server.js
+++ src/server.js
@@ -1,3 +1,10 @@
 import  express from "express";
 
+const POST = 4000;
+
 const app = express();
+
+const handleListening = () => 
+  console.log(`Server listening on port http://localhost:${POST}`);
+
+app.listen(POST, handleListening);
