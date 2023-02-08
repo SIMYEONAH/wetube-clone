c33b7399483443ed45ea028cb67fc0fe6cbaf017
SIMYEONAH <simya0528@naver.com>
2023-02-08 21:40:14 +0900

    Ay-Yo

--- src/server.js
+++ src/server.js
@@ -4,6 +4,17 @@ const POST = 4000;
 
 const app = express();
 
+const handleHome = (req, res) => {
+  return res.send("I still love you.");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("Login here.");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () => 
   console.log(`Server listening on port http://localhost:${POST}`);
 
