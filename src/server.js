2e0e89e9dab90f3334c9289d8e6de8d3bfda92ec
SIMYEONAH <simya0528@naver.com>
2023-02-09 23:25:04 +0900

    privateMiddleware

--- src/server.js
+++ src/server.js
@@ -4,8 +4,17 @@ const POST = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if(url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed you may continue.");
   next();
 };
 
@@ -13,7 +22,10 @@ const handleHome = (req, res) => {
   return res.send("I love middlewares");
 };
 
-app.get("/",gossipMiddleware, handleHome);
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
 
 const handleListening = () => 
   console.log(`Server listening on port http://localhost:${POST}`);
