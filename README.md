9b097b6bc09f90c1e2e737bfd3a1551f3a5322e8
SIMYEONAH <simya0528@naver.com>
2023-02-12 23:15:57 +0900

    routers
    Making our routers and README

---
+++ README.md
@@ -0,0 +1,16 @@
+#Wetude Reloaded
+
+/ -> HOME
+
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a Video
+/videos/comments/delete -> Delete a Comment of a Video
--- src/server.js
+++ src/server.js
@@ -1,33 +1,35 @@
-import  express from "express";
+import express from "express";
+import morgan from "morgan";
 
-const POST = 4000;
+const PORT = 4000;
 
 const app = express();
-
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if(url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed you may continue.");
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.send("I love middlewares");
-};
-
-
+const logger = morgan("dev");
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
 
-const handleListening = () => 
-  console.log(`Server listening on port http://localhost:${POST}`);
+const globalRouter = express.Router();
 
-app.listen(POST, handleListening);
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
