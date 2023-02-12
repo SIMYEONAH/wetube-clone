50c17e5da2b9d6e237abf09953ac345ae761c361
SIMYEONAH <simya0528@naver.com>
2023-02-13 02:50:10 +0900

    Partials and Extending Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,9 @@
-export const trending = (req, res) => res.send("Home page Video");
-export const see = (req, res) => res.render("home");
-
-export const edit = (req, res) => {
-  console.log(req.params);
-  return res.send("Edit")};
+export const trending = (req, res) => res.render("home");
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
-  console.log(req.params);
-  return res.send("Delete Video")};
+  return res.send("Delete Video");
+};
 
--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
+
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
-import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
+
+console.log(process.cwd());
 
 const app = express();
 const logger = morgan("dev");
 
-app.set("view engine","pug");
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
