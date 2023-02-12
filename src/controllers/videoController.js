33c0c92b71940ed02c9ebd9264973c377d80a10d
SIMYEONAH <simya0528@naver.com>
2023-02-13 00:30:52 +0900

    exports
    learning about exports

---
+++ src/controllers/videoController.js
@@ -0,0 +1,5 @@
+export const trending = (req, res) => res.send("Home page Video");
+
+export const watch = (req, res) => res.send("Watch");
+export const edit = (req, res) => res.send("Edit Video");
+
--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
@@ -7,23 +10,6 @@ const app = express();
 const logger = morgan("dev");
 app.use(logger);
 
-const globalRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send("Edit User");
-
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-videoRouter.get("/watch", handleWatchVideo);
 
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
