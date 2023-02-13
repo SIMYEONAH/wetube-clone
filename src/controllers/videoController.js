b73d2ebbff530c42da7f33c67cba23008c0cae7b
SIMYEONAH <simya0528@naver.com>
2023-02-13 23:38:56 +0900

    Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,32 @@
-export const trending = (req, res) =>{
-  const videos =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  res.render("home", { pageTitle: "Home", videos });
-}
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
@@ -9,4 +34,3 @@ export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
   return res.send("Delete Video");
 };
-
