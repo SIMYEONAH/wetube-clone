3a2f0b7bb17f10dc2796df3eb91bb609b8db4c9f
SIMYEONAH <simya0528@naver.com>
2023-02-13 23:23:46 +0900

    Iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,7 @@
-
-const fakeUser = {
-  username: "YEONAH",
-  loggedIn: false,
-};
-
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
+export const trending = (req, res) =>{
+  const videos =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  res.render("home", { pageTitle: "Home", videos });
+}
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
