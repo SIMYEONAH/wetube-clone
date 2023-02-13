28da24b56d6f4af93400fd83c02a3fe3b0ebb278
SIMYEONAH <simya0528@naver.com>
2023-02-13 22:46:32 +0900

    Conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,13 @@
-export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
-export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
-export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
+
+const fakeUser = {
+  username: "YEONAH",
+  loggedIn: false,
+};
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser });
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
